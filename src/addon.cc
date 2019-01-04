e37344cd32cd77c7ca2034dfb8d73f2d24d50950
Rob Loach <robloach@gmail.com>
2019-01-03 22:49:37 -0500

    Update

--- src/addon.cc
+++ src/addon.cc
@@ -14,13 +14,86 @@ NAN_METHOD(IsWindowReady) {
 	info.GetReturnValue().Set(IsWindowReady());
 }
 
-NAN_METHOD(Add) {
-  double value = info[0]->NumberValue() + info[1]->NumberValue();
-  info.GetReturnValue().Set(New(value));
+NAN_METHOD(WindowShouldClose) {
+	bool ret = WindowShouldClose();
+	info.GetReturnValue().Set(ret);
+}
+
+NAN_METHOD(IsWindowMinimized) {
+	info.GetReturnValue().Set(IsWindowMinimized());
+}
+
+NAN_METHOD(SetTargetFPS) {
+	SetTargetFPS(info[0]->IntegerValue());
+}
+
+NAN_METHOD(InitWindow) {
+	std::string title = std::string(*Nan::Utf8String(info[2]));
+	InitWindow(info[0]->IntegerValue(), info[1]->IntegerValue(), title.c_str());
+}
+
+NAN_METHOD(BeginDrawing) {
+	BeginDrawing();
+}
+
+NAN_METHOD(EndDrawing) {
+	EndDrawing();
+}
+
+NAN_METHOD(CloseWindow) {
+	CloseWindow();
+}
+
+NAN_METHOD(ClearBackground) {
+	ClearBackground(RAYWHITE);
+}
+
+NAN_METHOD(DrawText) {
+	std::string title = std::string(*Nan::Utf8String(info[0]));
+	DrawText(title.c_str(), info[1]->IntegerValue(), info[2]->IntegerValue(), info[3]->IntegerValue(), LIGHTGRAY);
 }
 
 NAN_MODULE_INIT(Init) {
-  Set(target, New("add").ToLocalChecked(), GetFunction(New<FunctionTemplate>(Add)).ToLocalChecked());
+	Set(target,
+		New("IsWindowReady").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(IsWindowReady)).ToLocalChecked()
+	);
+	Set(target,
+		New("WindowShouldClose").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(WindowShouldClose)).ToLocalChecked()
+	);
+	Set(target,
+		New("IsWindowMinimized").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(IsWindowMinimized)).ToLocalChecked()
+	);
+	Set(target,
+		New("SetTargetFPS").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(SetTargetFPS)).ToLocalChecked()
+	);
+	Set(target,
+		New("InitWindow").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(InitWindow)).ToLocalChecked()
+	);
+	Set(target,
+		New("BeginDrawing").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(BeginDrawing)).ToLocalChecked()
+	);
+	Set(target,
+		New("EndDrawing").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(EndDrawing)).ToLocalChecked()
+	);
+	Set(target,
+		New("CloseWindow").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(CloseWindow)).ToLocalChecked()
+	);
+	Set(target,
+		New("ClearBackground").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(ClearBackground)).ToLocalChecked()
+	);
+	Set(target,
+		New("DrawText").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(DrawText)).ToLocalChecked()
+	);
 }
 
 NODE_MODULE(addon, Init)
--- test/index.js
+++ test/index.js
@@ -1,7 +1,56 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2018 Rob Loach (@RobLoach)
+*
+********************************************************************************************/
+
+// Initialization
+//--------------------------------------------------------------------------------------
 var raylib = require('..')
+var screenWidth = 800
+var screenHeight = 450
 
-var out = raylib.add(3, 5)
-console.log('This should be eight: ', out)
+raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
 
-var oooo = raylib.IsWindowReady()
-console.log('Out: ',oooo ? 'True' : 'False')
+raylib.SetTargetFPS(60)  // Set target frames-per-second
+//--------------------------------------------------------------------------------------
+
+// Main game loop
+while (!raylib.WindowShouldClose())    // Detect window close button or ESC key
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    // TODO: Update your variables here
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    raylib.BeginDrawing()
+
+        raylib.ClearBackground(/*RAYWHITE*/)
+
+        raylib.DrawText("Congrats! You created your first window!", 190, 200, 20/*, LIGHTGRAY*/)
+
+    raylib.EndDrawing()
+    //----------------------------------------------------------------------------------
+}
+
+// De-Initialization
+//--------------------------------------------------------------------------------------
+raylib.CloseWindow()        // Close window and OpenGL context
+//--------------------------------------------------------------------------------------
