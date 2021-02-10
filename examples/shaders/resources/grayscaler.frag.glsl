dfdfc42fdd664f2dad646733fff487dbb9d7cfb0
Corey Sharrah <corey@eviltreehouse.com>
2021-02-10 14:24:56 -0700

    clean up frag shader

--- examples/shaders/resources/grayscaler.frag.glsl
+++ examples/shaders/resources/grayscaler.frag.glsl
@@ -6,20 +6,16 @@ in vec4 fragColor;
 
 // Input uniform values
 uniform sampler2D texture0;
-uniform vec4 colDiffuse;
+// lets test the uniform I/O to manipulate the ramp
+uniform float dt;
 
 // Output fragment color
 out vec4 finalColor;
 
-// NOTE: Add here your custom variables
-
-// lets test the uniform I/O to manipulate the ramp
-uniform float dt;
-
 void main()
 {
     // Texel color fetching from texture sampler
-    vec4 texelColor = texture(texture0, fragTexCoord)*colDiffuse*fragColor*dt;
+    vec4 texelColor = texture(texture0, fragTexCoord)*fragColor*dt;
     
     // Convert texel color to grayscale using NTSC conversion weights
     float gray = dot(texelColor.rgb, vec3(0.299, 0.587, 0.114));
