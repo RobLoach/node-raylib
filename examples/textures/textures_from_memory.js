e4bbb147643b83aa74afce89ee92292ac2060599
David Konsumer <konsumer@jetboystudio.com>
2022-02-22 12:37:04 -0800

    update demo to use index.js

--- examples/textures/textures_from_memory.js
+++ examples/textures/textures_from_memory.js
@@ -9,27 +9,27 @@
 *
 ********************************************************************************************/
 
-const r = require('raylib')
+const r = require('../../index.js')
 const { resolve } = require('path')
 const { readFileSync } = require('fs')
 
 // Initialization
-//--------------------------------------------------------------------------------------
-const screenWidth = 800;
-const screenHeight = 450;
+// --------------------------------------------------------------------------------------
+const screenWidth = 800
+const screenHeight = 450
 
 // Load the image using fs module into a `Buffer` instance.
 // NOTE: this `Buffer` could come from *anywhere* (statically encoded in your source code, as a
 // partial read from a custom resource packer file, etc.)
-const raw = readFileSync(resolve(__dirname, 'resources', 'wabbit_alpha.png'));
+const raw = readFileSync(resolve(__dirname, 'resources', 'wabbit_alpha.png'))
 
-r.InitWindow(screenWidth, screenHeight, "raylib [textures] example - image loading from memory");
+r.InitWindow(screenWidth, screenHeight, 'raylib [textures] example - image loading from memory')
 
 // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-const image = r.LoadImageFromMemory('png', raw, raw.length);     // Loaded in CPU memory (RAM)
+const image = r.LoadImageFromMemory('png', raw, raw.length) // Loaded in CPU memory (RAM)
 if (!image) {
-	console.error('image failed to load!');
-	process.exit(1);
+  console.error('image failed to load!')
+  process.exit(1)
 }
 
 // TODO: Fix thrown exception
@@ -37,33 +37,32 @@ if (!image) {
 // terminate called after throwing an instance of 'char const*'
 // [1]    26364 abort (core dumped)  bin/node-raylib examples/textures/textures_image_loading.js
 
-const texture = r.LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
+const texture = r.LoadTextureFromImage(image) // Image converted to texture, GPU memory (VRAM)
 
-r.UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
-//---------------------------------------------------------------------------------------
+r.UnloadImage(image) // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+// ---------------------------------------------------------------------------------------
 
 // Main game loop
-while (!r.WindowShouldClose())    // Detect window close button or ESC key
-{
-    // Update
+while (!r.WindowShouldClose()) { // Detect window close button or ESC key
+  // Update
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    r.BeginDrawing();
+  // Draw
+  // ----------------------------------------------------------------------------------
+  r.BeginDrawing()
 
-        r.ClearBackground(r.RAYWHITE);
+  r.ClearBackground(r.RAYWHITE)
 
-        r.DrawTexture(texture, screenWidth/2 - texture.width/2, screenHeight/2 - texture.height/2, r.WHITE);
+  r.DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2, r.WHITE)
 
-        r.DrawText("this IS a texture loaded from memory!", 300, 370, 10, r.GRAY);
+  r.DrawText('this IS a texture loaded from memory!', 300, 370, 10, r.GRAY)
 
-    r.EndDrawing();
-    //----------------------------------------------------------------------------------
+  r.EndDrawing()
+  // ----------------------------------------------------------------------------------
 }
 
 // De-Initialization
-//--------------------------------------------------------------------------------------
-r.UnloadTexture(texture);       // Texture unloading
+// --------------------------------------------------------------------------------------
+r.UnloadTexture(texture) // Texture unloading
 
-r.CloseWindow();                // Close window and OpenGL context
-//--------------------------------------------------------------------------------------
+r.CloseWindow() // Close window and OpenGL context
+// --------------------------------------------------------------------------------------
