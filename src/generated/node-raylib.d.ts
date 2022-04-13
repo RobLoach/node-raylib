f132b93144ed447612d1e5fd90eaa48681f5a889
tuckie <22203973+twuky@users.noreply.github.com>
2022-04-12 17:04:32 -0700

    Update typescript definition generator

--- src/generated/node-raylib.d.ts
+++ src/generated/node-raylib.d.ts
@@ -403,6 +403,18 @@ declare module "raylib" {
     /** VR distortion scale in */
     scaleIn: number
   }
+  
+  /** RenderTexture, fbo for texture rendering */
+  export type RenderTexture2D = RenderTexture
+
+  /** Texture, tex data stored in GPU memory (VRAM) */
+  export type Texture2D = Texture
+
+  /** Texture, tex data stored in GPU memory (VRAM) */
+  export type TextureCubemap = Texture
+
+  /** Camera, defines position/orientation in 3d space */
+  export type Camera = Camera3D | Camera2D
 
   /** Initialize window and OpenGL context */
   export function InitWindow(width: number, height: number, title: string): void
@@ -2406,30 +2418,30 @@ declare module "raylib" {
   /** Npatch layout: 3x1 tiles */
   export const NPATCH_THREE_PATCH_HORIZONTAL = 2
 
-  export const LIGHTGRAY: Color = { r: 200, g: 200, b: 200, a: 255 }
-  export const GRAY: Color = { r: 130, g: 130, b: 130, a: 255 }
-  export const DARKGRAY: Color = { r: 80, g: 80, b: 80, a: 255 }
-  export const YELLOW: Color = { r: 253, g: 249, b: 0, a: 255 }
-  export const GOLD: Color = { r: 255, g: 203, b: 0, a: 255 }
-  export const ORANGE: Color = { r: 255, g: 161, b: 0, a: 255 }
-  export const PINK: Color = { r: 255, g: 109, b: 194, a: 255 }
-  export const RED: Color = { r: 230, g: 41, b: 55, a: 255 }
-  export const MAROON: Color = { r: 190, g: 33, b: 55, a: 255 }
-  export const GREEN: Color = { r: 0, g: 228, b: 48, a: 255 }
-  export const LIME: Color = { r: 0, g: 158, b: 47, a: 255 }
-  export const DARKGREEN: Color = { r: 0, g: 117, b: 44, a: 255 }
-  export const SKYBLUE: Color = { r: 102, g: 191, b: 255, a: 255 }
-  export const BLUE: Color = { r: 0, g: 121, b: 241, a: 255 }
-  export const DARKBLUE: Color = { r: 0, g: 82, b: 172, a: 255 }
-  export const PURPLE: Color = { r: 200, g: 122, b: 255, a: 255 }
-  export const VIOLET: Color = { r: 135, g: 60, b: 190, a: 255 }
-  export const DARKPURPLE: Color = { r: 112, g: 31, b: 126, a: 255 }
-  export const BEIGE: Color = { r: 211, g: 176, b: 131, a: 255 }
-  export const BROWN: Color = { r: 127, g: 106, b: 79, a: 255 }
-  export const DARKBROWN: Color = { r: 76, g: 63, b: 47, a: 255 }
-  export const WHITE: Color = { r: 255, g: 255, b: 255, a: 255 }
-  export const BLACK: Color = { r: 0, g: 0, b: 0, a: 255 }
-  export const BLANK: Color = { r: 0, g: 0, b: 0, a: 0 }
-  export const MAGENTA: Color = { r: 255, g: 0, b: 255, a: 255 }
-  export const RAYWHITE: Color = { r: 245, g: 245, b: 245, a: 255 }
+  export const LIGHTGRAY: { r: 200, g: 200, b: 200, a: 255 }
+  export const GRAY: { r: 130, g: 130, b: 130, a: 255 }
+  export const DARKGRAY: { r: 80, g: 80, b: 80, a: 255 }
+  export const YELLOW: { r: 253, g: 249, b: 0, a: 255 }
+  export const GOLD: { r: 255, g: 203, b: 0, a: 255 }
+  export const ORANGE: { r: 255, g: 161, b: 0, a: 255 }
+  export const PINK: { r: 255, g: 109, b: 194, a: 255 }
+  export const RED: { r: 230, g: 41, b: 55, a: 255 }
+  export const MAROON: { r: 190, g: 33, b: 55, a: 255 }
+  export const GREEN: { r: 0, g: 228, b: 48, a: 255 }
+  export const LIME: { r: 0, g: 158, b: 47, a: 255 }
+  export const DARKGREEN: { r: 0, g: 117, b: 44, a: 255 }
+  export const SKYBLUE: { r: 102, g: 191, b: 255, a: 255 }
+  export const BLUE: { r: 0, g: 121, b: 241, a: 255 }
+  export const DARKBLUE: { r: 0, g: 82, b: 172, a: 255 }
+  export const PURPLE: { r: 200, g: 122, b: 255, a: 255 }
+  export const VIOLET: { r: 135, g: 60, b: 190, a: 255 }
+  export const DARKPURPLE: { r: 112, g: 31, b: 126, a: 255 }
+  export const BEIGE: { r: 211, g: 176, b: 131, a: 255 }
+  export const BROWN: { r: 127, g: 106, b: 79, a: 255 }
+  export const DARKBROWN: { r: 76, g: 63, b: 47, a: 255 }
+  export const WHITE: { r: 255, g: 255, b: 255, a: 255 }
+  export const BLACK: { r: 0, g: 0, b: 0, a: 255 }
+  export const BLANK: { r: 0, g: 0, b: 0, a: 0 }
+  export const MAGENTA: { r: 255, g: 0, b: 255, a: 255 }
+  export const RAYWHITE: { r: 245, g: 245, b: 245, a: 255 }
 }
