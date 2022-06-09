c193e4036a9e9ea06c6fb01b731c4ee3cd302d17
David Konsumer <konsumer@jetboystudio.com>
2022-06-08 22:20:54 -0700

    add crossbuild

--- tools/crossbuild.sh
+++ tools/crossbuild.sh
@@ -3,7 +3,7 @@
 # this is used inside a docker (running with qemu binfmt) to easily crossbuild project
 # run with docker run --platform linux/arm64 -it --rm -v "${PWD}:/work" -w /work node ./tools/crossbuild.sh
 
-apt update
-apt install -y xorg-dev libglu1-mesa-dev
-npm i
+apt-get update
+apt-get install -y xorg-dev libglu1-mesa-dev cmake
+npm ci
 npm run compile
