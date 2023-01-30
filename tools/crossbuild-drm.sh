08f4edb9c39ca69566f9f40c59a5938e3e2b184d
tuckie <hey@tuckie.zone>
2023-01-29 16:35:12 -0800

    crossbuild-drm installs drm deps

--- tools/crossbuild-drm.sh
+++ tools/crossbuild-drm.sh
@@ -4,7 +4,7 @@
 # run with docker run --platform linux/arm64 -it --rm -v "${PWD}:/work" -w /work node ./tools/crossbuild.sh
 
 apt-get update
-apt-get install -y xorg-dev libglu1-mesa-dev cmake
+apt-get install -y xorg-dev libglu1-mesa-dev cmake libdrm-dev libegl1-mesa-dev libgles2-mesa-dev libgbm-dev
 # npm ci
 # rm -r build
 npm run compile-drm
